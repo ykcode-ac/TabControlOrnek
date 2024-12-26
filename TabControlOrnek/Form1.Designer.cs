78669a4835e2600637d21065b3fe6d4ddcbdb117
ahmetc <ahmetc@MSIAC>
2024-12-26 16:09:51 +0300

    Add project files.

---
+++ TabControlOrnek/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace TabControlOrnek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDownCorba = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownIskender = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDownKofte = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDownAyran = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDownTatli = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCorba)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownIskender)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownKofte)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAyran)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTatli)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(683, 367);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 33);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(675, 330);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Müşteri Bilgileri";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.numericUpDownTatli);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.numericUpDownAyran);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.numericUpDownKofte);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.numericUpDownIskender);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.numericUpDownCorba);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 33);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(675, 330);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Sipariş Bilgileri";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.label1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 33);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(675, 330);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Sipariş Özeti";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(248, 151);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(255, 29);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Müşteri";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(188, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Çorba";
+            // 
+            // numericUpDownCorba
+            // 
+            this.numericUpDownCorba.Location = new System.Drawing.Point(291, 60);
+            this.numericUpDownCorba.Name = "numericUpDownCorba";
+            this.numericUpDownCorba.Size = new System.Drawing.Size(120, 29);
+            this.numericUpDownCorba.TabIndex = 1;
+            // 
+            // numericUpDownIskender
+            // 
+            this.numericUpDownIskender.Location = new System.Drawing.Point(291, 95);
+            this.numericUpDownIskender.Name = "numericUpDownIskender";
+            this.numericUpDownIskender.Size = new System.Drawing.Size(120, 29);
+            this.numericUpDownIskender.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(188, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 24);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "İskender";
+            // 
+            // numericUpDownKofte
+            // 
+            this.numericUpDownKofte.Location = new System.Drawing.Point(291, 130);
+            this.numericUpDownKofte.Name = "numericUpDownKofte";
+            this.numericUpDownKofte.Size = new System.Drawing.Size(120, 29);
+            this.numericUpDownKofte.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(188, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Köfte";
+            // 
+            // numericUpDownAyran
+            // 
+            this.numericUpDownAyran.Location = new System.Drawing.Point(291, 165);
+            this.numericUpDownAyran.Name = "numericUpDownAyran";
+            this.numericUpDownAyran.Size = new System.Drawing.Size(120, 29);
+            this.numericUpDownAyran.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(188, 167);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 24);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Ayran";
+            // 
+            // numericUpDownTatli
+            // 
+            this.numericUpDownTatli.Location = new System.Drawing.Point(291, 200);
+            this.numericUpDownTatli.Name = "numericUpDownTatli";
+            this.numericUpDownTatli.Size = new System.Drawing.Size(120, 29);
+            this.numericUpDownTatli.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(188, 202);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 24);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Tatlı";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 367);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCorba)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownIskender)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownKofte)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAyran)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTatli)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDownTatli;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown numericUpDownAyran;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDownKofte;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDownIskender;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDownCorba;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TabControlOrnek/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TabControlOrnek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private readonly decimal[] yemekFiyatlari = { 20m, 50m, 40m, 10m, 25m };
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // TabPage3'e geçildiğinde hesap özeti oluşturulur
+            if (tabControl1.SelectedIndex == 2)
+            {
+                string musteriAdi = textBox1.Text;
+                decimal toplamTutar = 0;
+                string[] yemekler = { "Çorba", "İskender", "Köfte", "Ayran", "Tatlı" };
+
+                string hesapOzeti = $"Müşteri Adı: {musteriAdi}\n\nSipariş Özeti:\n----------------------\n";
+
+                // Her bir yemeğin adet ve tutar bilgisi
+                NumericUpDown[] adetler = { numericUpDownCorba, numericUpDownIskender, numericUpDownKofte, numericUpDownAyran, numericUpDownTatli };
+
+                for (int i = 0; i < yemekler.Length; i++)
+                {
+                    int adet = (int)adetler[i].Value;
+                    decimal tutar = adet * yemekFiyatlari[i];
+                    toplamTutar += tutar;
+
+                    if (adet > 0)
+                    {
+                        hesapOzeti += $"{yemekler[i]} x {adet} adet = {tutar:C}\n";
+                    }
+                }
+
+                hesapOzeti += $"\nToplam Tutar: {toplamTutar:C}";
+
+                // Hesap özetini label1'e yazdır
+                label1.Text = hesapOzeti;
+            }
+        }
+    }
+}
